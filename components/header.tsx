758519dee70db9692027dcd42cb2dbf76e32127a
joshuscurtis <joshuscurtis@gmail.com>
2021-04-04 00:07:39 +0100

    test

--- components/header.tsx
+++ components/header.tsx
@@ -2,21 +2,21 @@ import header from "../styles/Header.module.css";
 import Link from "next/link";
 export default function Header() {
   return (
-    <div class={header.container}>
-      <div class={header.logo}>LOGO</div>
-      <div class={header.items}>
+    <div className={header.container}>
+      <div className={header.logo}>LOGO</div>
+      <div className={header.items}>
         <Link href="/">
-          <div class={header.item}>Home</div>
+          <div className={header.item}>Home</div>
         </Link>
 
         <Link href="french">
-          <div class={header.item}>Learn French</div>
+          <div className={header.item}>Learn French</div>
         </Link>
         <Link href="english">
-          <div class={header.item}>Learn English</div>
+          <div className={header.item}>Learn English</div>
         </Link>
         <Link href="about">
-          <div class={header.item}>About Us</div>
+          <div className={header.item}>About Us</div>
         </Link>
       </div>
     </div>
